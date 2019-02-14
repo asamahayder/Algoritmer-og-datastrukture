1395838416dbcd4897ab0943f10ee327526b619a
Duyx <sandersen98@live.dk>
2019-02-14 08:55:57 +0100

    Hej

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        int[] arrayAL = {1, 3, 7, 15, 17, 11, 2, 3, 6, 8, 7, 5, 9, 5, 23};
+
+        int n =arrayAL.length;
+
+        if (arrayAL[0] >= arrayAL[1]){
+            return;
+        }
+
+        for(int i = 1; i < n-2 ; i++){
+            if (arrayAL[i-1] <= arrayAL[i] => arrayAL[i+1]) {
+
+            }
+        }
 
     }
 }
+
+    peak1(A, n){
+        if (A[0] => A[1]) {
+            return;
+        }
+
+
+    }
